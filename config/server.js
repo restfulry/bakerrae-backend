bee4097da041588c6470e6a74172f3829cbc36c0
Ryan Lee <ryan@RY-MBP-TB-Local>
2022-02-04 19:35:31 -0500

    server js url to api.bakerrae.com - run build

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost:1337'),
+  url: env('URL', 'http://api.bakerrae.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'df67d1a842f5996e532fbc0d343ebfae'),
